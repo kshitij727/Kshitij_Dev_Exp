cb37e1643cbb4ce17060773191699adb1aa38f4d
kshitij727 <kingkshitij727@gmail.com>
2026-07-13 09:43:36 +0530

    hello.c

--- hello.c
+++ hello.c
@@ -4,3 +4,16 @@ int main() {
     printf("Hello, World!\n");
     return 0;
 }
+
+
+
+
+
+int main() {
+    int a = 5;
+    int b = 10;
+    int sum = a + b;
+
+    printf("The sum of %d and %d is: %d\n", a, b, sum);
+    return 0;
+}
